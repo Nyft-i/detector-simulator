80506ce23627d3b669c2527ee22dbd11039fa386
Tom Salisbury <thomas.salisbury-2@student.manchester.ac.uk>
2024-04-28 19:29:09 +0100

    nucleon rule of five

--- nucleon.cpp
+++ nucleon.cpp
@@ -48,8 +48,78 @@ Nucleon::Nucleon(double con_energy, string con_nuc_type)
   }
 }
 
+// Copy
+Nucleon::Nucleon(const Nucleon& copy_from)
+{
+  true_energy = copy_from.true_energy;
+  rest_mass = copy_from.rest_mass;
+  charge = copy_from.charge;
+  name = copy_from.name;
+  nuc_type = copy_from.nuc_type;
+}
+
+// Move
+Nucleon::Nucleon(Nucleon&& move_from)
+{
+  true_energy = move_from.true_energy;
+  rest_mass = move_from.rest_mass;
+  charge = move_from.charge;
+  name = std::move(move_from.name);
+  nuc_type = std::move(move_from.nuc_type);
+
+  // Set attributes of move_from to nothing.
+  move_from.true_energy = 0;
+  move_from.rest_mass = 0;
+  move_from.charge = 0;
+  move_from.name = "";
+  move_from.nuc_type = "";
+}
+
+// Destructor 
+
+// Assignment
+// Copy
+Nucleon& Nucleon::operator=(const Nucleon& copy_from)
+{
+  // Make sure we're not copying ourself.
+  if(&copy_from==this) return *this;
+  
+  // Copy the data
+  true_energy = copy_from.true_energy;
+  rest_mass = copy_from.rest_mass;
+  charge = copy_from.charge;
+  name = copy_from.name;
+  nuc_type = copy_from.nuc_type;
+
+  return *this;
+}
+
+// Move
+Nucleon& Nucleon::operator=(Nucleon&& move_from)
+{
+  // Make sure we're not moving from ourself.
+  if(&move_from == this) return *this;
+  
+  // Move the data
+  true_energy = move_from.true_energy;
+  rest_mass = move_from.rest_mass;
+  charge = move_from.charge;
+  name = std::move(move_from.name);
+  nuc_type = std::move(move_from.nuc_type);
+
+  // Set attributes of move_from to nothing.
+  move_from.true_energy = 0;
+  move_from.rest_mass = 0;
+  move_from.charge = 0;
+  move_from.name = "";
+  move_from.nuc_type = "";
+
+  return *this;
+}
+
 // Functionality
 void Nucleon::print()
 {
-  
+  std::cout<<"particle is : "<<nuc_type<<std::endl;
+  DepositorParticle::print();
 }
